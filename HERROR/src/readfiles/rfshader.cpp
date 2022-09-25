6eec734926591db3916379e4d23e0ee91a43fc27
XI-TANG <mobai2209122642@gmail.com>
2022-09-25 12:22:03 +0800

    完善文件流操作

--- HERROR/src/readfiles/rfshader.cpp
+++ HERROR/src/readfiles/rfshader.cpp
@@ -1,27 +1,57 @@
-#include "rfshader.h"
+﻿#include "rfshader.h"
 #include <fstream>
-
-
+#include <sstream>
+#include <iostream>
+/*
+* c++中的文件流：
+*	file in disk -> file buffer -> string buffer -> string -> char*
+*/
 rfshader::rfshader(const char* vertexPath, const char* fragmentPath) 
 {
-	std::ifstream vertexFile;
-	std::ifstream fragmentFile;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
 
-	vertexFile.open(vertexPath);
-	fragmentFile.open(fragmentPath);
+	vShaderFile.open(vertexPath);
+	fShaderFile.open(fragmentPath);
+
+	// 保证ifstream对象可以抛出异常：
+	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 
 	try
 	{
-		if (!vertexFile.is_open()) {
+		//打开文件
+		if (!vShaderFile.is_open()) {
 			throw std::exception("open vertex file err");
 		}
-		if (!fragmentFile.is_open()) {
+		if (!fShaderFile.is_open()) {
 			throw std::exception("open fragment file err");
 		}
+
+		std::stringstream vShaderStream, fShaderStream;
+		// 读取文件的缓冲内容到数据流中
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
+
+		// 关闭文件处理器
+		vShaderFile.close();
+		fShaderFile.close();
+
+		// 转换数据流到string
+		vertexCodeString = vShaderStream.str();
+		fragmentCodeString = fShaderStream.str();
+
+		//string to char*
+		vShaderCode = vertexCodeString.c_str();
+		fShaderCode = fragmentCodeString.c_str();
+
+		printf(vShaderCode);
+		printf(fShaderCode);
 	}
 	catch (const std::exception& ex)
 	{
 		printf(ex.what());
 		printf("\t\n");
+		printf("ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ\t\n");
 	}
 }
--- HERROR/src/readfiles/rfshader.h
+++ HERROR/src/readfiles/rfshader.h
@@ -1,10 +1,16 @@
-#pragma once
+﻿#pragma once
+#include <string>
 
 /* read shader from files */
 class rfshader
 {
 public:
 	rfshader(const char* vertexPath, const char* fragmentPath);
+	
+	std::string vertexCodeString;
+	std::string fragmentCodeString;
+	const char* vShaderCode = "";//用于储存从文件读取的源码
+	const char* fShaderCode = "";
 private:
 };
 
